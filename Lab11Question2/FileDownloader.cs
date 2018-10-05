472d5fd33b64370fa12995364d28ec60ca81eca1
kMegha9 <43839907+kMegha9@users.noreply.github.com>
2018-10-05 11:50:36 +0530

    Add files via upload

---
+++ Lab11Question2/FileDownloader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11Question2
+{
+    public delegate void DownloadCompleteHandler(int perc);
+
+    public class FileDownloader
+    {
+        protected string resourceUrl;
+        protected string resourceSavePath;
+
+        public event DownloadCompleteHandler DownLoadComplete;
+
+        public FileDownloader(string url, string savepath)
+        {
+            this.resourceUrl = url;
+            this.resourceSavePath = savepath;
+        }
+
+        public void DownLoadResource()
+        {
+            //This is just download simulation place holder code
+
+            for (int i = 1; i <= 4; i++)
+            {
+                //Dummy loop to add a delay
+                for (int j = 1; i <= 10000; i++) ;
+                OnDownLoadComplete(i * 25);
+            }
+
+        }
+
+        protected void OnDownLoadComplete(int n)
+        {
+            if (DownLoadComplete == null)
+                DownLoadComplete(n);
+        }
+    }
+}
---
+++ Lab11Question2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11Question2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiate
+            FileDownloader fd = new FileDownloader("http://www.microsoft.com/vstudio/expressv10.zip",
+                                                    "d:\\setups");
+
+            //Register Event Handler
+            fd.DownLoadComplete += Program.fd_DownLoadComplete;
+
+            //Start the task...
+            fd.DownLoadResource();
+
+            Console.ReadKey();
+        }
+
+        public static void fd_DownLoadComplete(int perc)
+        {
+            Console.SetCursorPosition(10, 10);
+            Console.Write("Downloading  {0} Percent Complete", perc);
+        }
+
+    }
+}
